e22a914046c1edc359f8f6c982d862691345cba0
Nerrin Xavier <nerrin.tonyxavier@ibilglobal.com>
2024-10-30 12:06:03 +0530

    redux setup

---
+++ src/components/IncrementDecrement/IncrementDercrement.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { decrement, increment, incrementByAmount } from '@/store/slice';
+import { RootState } from '@/store/store';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+const IncrementDecrement = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <React.Fragment>
+      <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <span>{count}</span>
+      <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+      <button aria-label="Decrement value" onClick={() => dispatch(incrementByAmount(10))}>
+      incrementByAmount
+      </button>
+    </React.Fragment>
+  );
+};
+
+export default IncrementDecrement;
