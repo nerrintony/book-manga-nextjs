cca2e02d7596eb6fac1d061d0a3d9d4ba814fff0
Nerrin Xavier <nerrin.tonyxavier@ibilglobal.com>
2024-11-20 11:11:39 +0530

    redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import Image from 'next/image';
-import styles from './page.module.css';
-import { useEffect } from 'react';
-import MangeVerseClient from '@/client/mangaverse.client';
-import axios from 'axios';
-import IncrementDecrement from '@/components/IncrementDecrement/IncrementDercrement';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return (
-    <div>
-      <IncrementDecrement />
-    </div>
-  );
+export default async function Index() {
+  redirect('/home');
 }
